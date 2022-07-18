8be1cf9484b50ebca0b5c358e6cade2d712f2baf
Noam Bendelac <noam.bendelac@willowtreeapps.com>
2022-07-18 14:23:22 -0400

    Some css layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,47 +3,59 @@ import logo from './logo.svg';
 import styles from './App.module.css';
 
 
-const Icon = ({ src }: { src: string }) => {
-  
-  
-  // const logo = props.logo
-  return <img src={src} />
-}
 
-
-
-const Counter = () => {
-  // let index = 0
-  const [index, setIndex] = useState(0)
-  console.log(`rerendering with index: ${index}`)
-  return <div>
-    <p>{index}</p>
-    <button onClick={() => { setIndex(index + 1) }}>+</button>
-  </div>
-}
 
 
 function App() {
   return (
     <div className={styles.app}>
-      <header className={styles.appHeader}>
-        <img src={logo} className={styles.appLogo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className={styles.appLink}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          testing
-        </a>
-        {/* <Icon    src={logo}    /> */}
-        <Counter />
-        <Counter />
-      </header>
+      <div className={styles.header}>
+        <div className={styles.headerColumn1}>
+          App Logo and Name
+        </div>
+        <div className={styles.headerColumn2}>
+          User Identification
+        </div>
+        <div className={styles.headerColumn3}>
+          Points
+        </div>
+      </div>
+      
+      <div className={styles.main}>
+        <div className={styles.mainRow1}>
+        
+          <h1 className={styles.question}>Who was the 16th president of the United States?</h1>
+          
+          <ol className={styles.answerChoices}>
+            {['A', 'B', 'C', 'D'].map(choice =>
+              <li className={styles.answerChoice}>
+                {choice}
+              </li>
+            )}
+          </ol>
+        </div>
+
+        
+        
+        
+      </div>
     </div>
+    // <div className={styles.app}>
+    //   <header className={styles.appHeader}>
+    //     <img src={logo} className={styles.appLogo} alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className={styles.appLink}
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       testing
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
