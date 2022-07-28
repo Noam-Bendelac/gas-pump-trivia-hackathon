2a6eef44de0bf13fc1a6f845e754615363b1c329
Elizabeth Palmieri <elizabeth.palmieri@willowtreeapps.com>
2022-07-28 12:07:43 -0400

    Co-authored-by: Maggie Munday <maggiemunday@users.noreply.github.com>

---
+++ client/src/questions.ts
@@ -0,0 +1,8 @@
+const q1 = "Who is your favorite celebrity?"
+
+const answers1: {"ans_id": string, "ans": string, "ans_status": boolean } [] = [
+    {"ans_id": "A", "ans": "Lady Gaga", "ans_status": false}
+]
+
+
+export default questions;
